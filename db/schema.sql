2087bf00ee56b5599d848f05cfd390fce0b3cefc
Emoses95 <moses.e.collins@gmail.com>
2023-02-20 19:12:03 -0800

    added to schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,17 @@
-DROP DATABASE IF EXISTS department_db;
-CREATE DATABASE department_db;
-USE department_db;
+DROP DATABASE IF EXISTS employeeTracker_db;
+CREATE DATABASE employeeTracker_db;
+USE employeeTracker_db;
 
-CREATE TABLE deparment (
+CREATE TABLE department (
     id NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
-    salary DECIMAL,
-    deparment_id INT
+    salary DECIMAL(6,2),
+    department_id INT
 );
 
 CREATE TABLE employee(
